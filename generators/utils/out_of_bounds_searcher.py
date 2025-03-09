1f8679d564825580d599df8a5d22257cc2d05c19
Ларукова Алина <112629460+Alinalark@users.noreply.github.com>
2025-03-09 20:00:09 +0300

    Исправлена обработка строк кода с циклом, часть кода вынесена в функцию

--- generators/utils/out_of_bounds_searcher.py
+++ generators/utils/out_of_bounds_searcher.py
@@ -1,10 +1,13 @@
 import re
 
 def find_potential_array_out_of_bounds(c_code):
+    '''Функция ищет некорректный код с ошибкой в цикле for'''
     # Найти объявления массивов
     array_declarations = {}  # Словарь для хранения имен массивов и их размеров
     array_declarations_use = {}  # Словарь для хранения присвоений массивов
     issues = {}  # Словарь для хранения найденных проблем
+    lines_and_issues = {} # Словарь для хранения найденных проблем и номеров строк
+    boundaries = 1 # Число для определения пустого условия
 
     # Разбиваем код на строки
     lines = c_code.splitlines()
@@ -35,54 +38,67 @@ def find_potential_array_out_of_bounds(c_code):
                     array_declarations_use[array_name_use] = parts
             else:
                 array_declarations_use[array_name_use] = index_expr
-            
-
-    # Проверка диапазонов индексов в циклах
-    for line_num, line in enumerate(lines):
+    
+    # Регулярное выражение для поиска условий цикла for
+    pattern = r'for\s*\((.*?)\)\s*{'
+    # Найти все совпадения
+    matches = re.findall(pattern, c_code, re.DOTALL)
+    # Список для хранения найденных условий с ошибкой
+    conditions = []
+    
+    for match in matches:
         errors = []  # Список для хранения ошибок в текущей строке
-        # Паттерн для поиска циклов for
-        for_loop = re.search(r'for\s*\(([^)]+)\)', line)
-            
-        if for_loop:
-            # Извлечение и анализ переменной цикла
-            loop_var = re.search(r'(\w+)\s*=\s*([^;]+)', for_loop.group(1))
-            if loop_var:
-                loop_index = loop_var.group(1).strip()
-                # Определение границ цикла (начальное и конечное значения)
-                boundaries = re.findall(r'-?\d+', for_loop.group(1))
-                if boundaries:
-                    start_index = int(boundaries[0])
-                    end_index = int(boundaries[-1])
+        # Извлечение переменной цикла
+        loop_index = match.split('=')[0].strip()
+        # Находит числа после знака '='
+        assignment_number = re.search(r'=\s*([-+]?\d+)', match)
+        # Находит числа после знаков сравнения
+        comparison_number = re.search(r'([<>]=?)\s*([-+]?\d+)', match)
+        # Определение границ цикла (начальное и конечное значения)
+        if assignment_number and comparison_number:
+            start_index = int(assignment_number.group(1))
+            end_index = int(comparison_number.group(2))
+            boundaries = 2
+        elif assignment_number:
+            start_index = int(assignment_number.group(1))
+            end_index = start_index
+            boundaries = 1
+        for array_name, array_size in array_declarations.items():
+            # Проверка индексов на выход за границы
+            if loop_index in array_declarations_use[array_name]:
+                if boundaries == 1:
+                    if start_index < 0 or start_index >= array_size:
+                        probl = f"начальный индекс {start_index} за пределами массива {array_name}, пустое условие выхода из цикла"
+                        errors.append(probl)
+                    else:
+                        errors.append("пустое условие выхода из цикла")
+                else:
+                    if (start_index < 0 or start_index >= array_size) and (end_index < 0 or end_index >= array_size):
+                        probl = f"начальный {start_index} и конечный {end_index} индексы за пределами массива {array_name}"
+                        errors.append(probl)
+                    elif start_index < 0 or start_index >= array_size:
+                        probl = f"начальный индекс {start_index} за пределами массива {array_name}"
+                        errors.append(probl)
+                    elif end_index < 0 or end_index >= array_size:
+                        probl = f"конечный индекс {end_index} за пределами массива {array_name}"
+                        errors.append(probl)
+            # Если есть ошибки, добавляем их в словарь
+            if errors:
+                if match not in conditions:
+                    conditions.append(match)
+                issues[match] = errors
 
-                    for array_name, array_size in array_declarations.items():
-                        # Проверка индексов на выход за границы
-                        if loop_index in array_declarations_use[array_name]:
-                            if len(boundaries) == 1:
-                                if start_index < 0 or start_index >= array_size:
-                                    probl = f"начальный индекс {start_index} за пределами массива {array_name}, пустое условие выхода из цикла"
-                                    errors.append(probl)
-                                else:
-                                    errors.append("пустое условие выхода из цикла")
-                            else:
-                                if (start_index < 0 or start_index >= array_size) and (end_index < 0 or end_index >= array_size):
-                                    probl = f"начальный {start_index} и конечный {end_index} индексы за пределами массива {array_name}"
-                                    errors.append(probl)
-                                elif start_index < 0 or start_index >= array_size:
-                                    probl = f"начальный индекс {start_index} за пределами массива {array_name}"
-                                    errors.append(probl)
-                                elif end_index < 0 or end_index >= array_size:
-                                    probl = f"конечный индекс {end_index} за пределами массива {array_name}"
-                                    errors.append(probl)
-                        # Если есть ошибки, добавляем их в словарь
-                        if errors:
-                            issues[line_num] = errors
-    return issues
+    # Добавляем в словарь номера строк в коде и соответствующую ошибку
+    for i, line in enumerate(lines):
+        for error in conditions:
+            if error in line:
+                lines_and_issues[i + 1] = issues[error]
+
+    return lines_and_issues
 
 
-if __name__ == "__main__":
-    # Статически заданное имя файла
-    filename = "generated_code_with_cycle.c"  # Имя C-файла
-
+def find_potential_errors_array_out_of_bounds(filename):
+    '''Функция считывает содержимое файла с кодом и запускает функцию поиска ошибки'''
     try:
         # Читаем содержимое файла
         with open(filename, "r") as f:
@@ -95,7 +111,7 @@ if __name__ == "__main__":
             print("Обнаружены потенциальные проблемы:")
             for line, description in issues_in_code.items():
                 # Выводим номер строки с ошибкой
-                print("Строка с ошибкой:", line + 1)
+                print("Строка с ошибкой:", line)
                 for issue in description:
                     # Выводим описание каждой проблемы
                     print(f"Описание: {issue}")
@@ -106,3 +122,7 @@ if __name__ == "__main__":
     except FileNotFoundError:
         print(f"Файл '{filename}' не найден.")
 
+
+if __name__ == "__main__":
+    filename = "generated_code_with_cycle.c"
+    find_potential_errors_array_out_of_bounds(filename)
